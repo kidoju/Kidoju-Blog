932f6945abb95769101f2b3417eb4d4a64639ae3
jlchereau <jlchereau@msn.com>
2015-06-10 09:10:19 +0100

    Updated with graphics copied to webapp/public

--- update.cmd
+++ update.cmd
@@ -1,10 +1,80 @@
 cd /d %~dp0
 REM ************************************************************
 REM IMPORTANT!
-REM Memba.Blog is the original project where any modifications
+REM Memba.Blog and Kidoju.WebApp are the original projects where any modifications
 REM of locked read-only files should be completed.
 REM ************************************************************
 
+REM ------------------------------------------------------------
+REM Graphics
+REM ------------------------------------------------------------
+
+ATTRIB -R .\graphics\*
+COPY ..\..\Kidoju\Kidoju.Webapp\graphics\*.ai .\graphics /Y
+ATTRIB +R .\graphics\*.ai
+COPY ..\..\Kidoju\Kidoju.Webapp\graphics\*.ico .\graphics /Y
+ATTRIB +R .\graphics\*.ico
+COPY ..\..\Kidoju\Kidoju.Webapp\graphics\*.png .\graphics /Y
+ATTRIB +R .\graphics\*.png
+COPY ..\..\Kidoju\Kidoju.Webapp\graphics\*.svg .\graphics /Y
+ATTRIB +R .\graphics\*.svg
+COPY ..\..\Kidoju\Kidoju.Webapp\graphics\*.ttf .\graphics /Y
+ATTRIB +R .\graphics\*.ttf
+
+ATTRIB -R .\webapp\public\apple*.png
+COPY .\graphics\apple*.png .\webapp\public /Y
+ATTRIB +R .\webapp\public\apple*.png
+
+ATTRIB -R .\webapp\public\favicon.ico
+COPY .\graphics\favicon.ico .\webapp\public /Y
+ATTRIB +R .\webapp\public\favicon.ico
+
+REM ------------------------------------------------------------
+REM Javascript files
+REM ------------------------------------------------------------
+
+REM Copy Javascript files
+XCOPY ..\..\Memba\Memba.Blog\js .\js /C /E /I /R /Y
+ATTRIB +R .\js\*.* /S
+
+REM ------------------------------------------------------------
+REM Styles
+REM ------------------------------------------------------------
+
+REM Copy styles
+XCOPY ..\..\Memba\Memba.Blog\styles .\styles /C /E /I /R /Y
+COPY .\graphics\kidoju.home.jpg .\styles\images\jumbotron.jpg
+ATTRIB +R .\styles\*.* /S
+
+REM ------------------------------------------------------------
+REM Tests
+REM ------------------------------------------------------------
+
+REM Copy tests
+XCOPY ..\..\Memba\Memba.Blog\test .\test /C /E /I /R /Y
+ATTRIB +R .\test\*.* /S
+
+REM ------------------------------------------------------------
+REM Web modules (webpack)
+REM ------------------------------------------------------------
+
+REM Copy web modules
+XCOPY ..\..\Memba\Memba.Blog\web_modules .\web_modules /C /E /I /R /Y
+ATTRIB +R .\web_modules\*.* /S
+
+REM ------------------------------------------------------------
+REM Web Application
+REM ------------------------------------------------------------
+
+ATTRIB -R .\webapp\*.* /S
+XCOPY ..\..\Memba\Memba.Blog\webapp .\webapp /C /E /I /R /Y /EXCLUDE:excludelist.txt
+COPY ..\..\Kidoju\Kidoju.Webapp\webapp\views\gtm.ejs .\webapp\views /Y
+COPY ..\..\Kidoju\Kidoju.Webapp\webapp\views\logo.ejs .\webapp\views /Y
+ATTRIB +R .\webapp\*.* /S
+ATTRIB -R .\webapp\cache\*.*
+ATTRIB -R .\webapp\config\*.json
+ATTRIB -R .\webapp\public\assets\*.*
+ATTRIB -R .\webapp\views\home.ejs
 
 REM ------------------------------------------------------------
 REM Root files
@@ -51,52 +121,3 @@ REM webpack
 ATTRIB -R .\webpack.config.js
 COPY ..\..\Memba\Memba.Blog\webpack.config.js .\ /Y
 ATTRIB +R .\webpack.config.js
-
-REM ------------------------------------------------------------
-REM Javascript files
-REM ------------------------------------------------------------
-
-REM Copy Javascript files
-XCOPY ..\..\Memba\Memba.Blog\js .\js /C /E /I /R /Y
-ATTRIB +R .\js\*.* /S
-
-REM ------------------------------------------------------------
-REM Styles
-REM ------------------------------------------------------------
-
-REM Copy styles
-XCOPY ..\..\Memba\Memba.Blog\styles .\styles /C /E /I /R /Y
-COPY .\graphics\kidoju.home.jpg .\styles\images\jumbotron.jpg
-ATTRIB +R .\styles\*.* /S
-
-REM ------------------------------------------------------------
-REM Tests
-REM ------------------------------------------------------------
-
-REM Copy tests
-XCOPY ..\..\Memba\Memba.Blog\test .\test /C /E /I /R /Y
-ATTRIB +R .\test\*.* /S
-
-REM ------------------------------------------------------------
-REM Web modules (webpack)
-REM ------------------------------------------------------------
-
-REM Copy web modules
-XCOPY ..\..\Memba\Memba.Blog\web_modules .\web_modules /C /E /I /R /Y
-ATTRIB +R .\web_modules\*.* /S
-
-
-REM ------------------------------------------------------------
-REM Web Application
-REM ------------------------------------------------------------
-
-XCOPY ..\..\Memba\Memba.Blog\webapp .\webapp /C /E /I /R /Y /EXCLUDE:excludelist.txt
-ATTRIB +R .\webapp\*.* /S
-ATTRIB -R .\webapp\cache\*.*
-ATTRIB -R .\webapp\config\*.json
-ATTRIB -R .\webapp\public\*.png
-ATTRIB -R .\webapp\public\*.ico
-ATTRIB -R .\webapp\public\assets\*.*
-ATTRIB -R .\webapp\views\gtm.ejs
-ATTRIB -R .\webapp\views\home.ejs
-ATTRIB -R .\webapp\views\logo.ejs

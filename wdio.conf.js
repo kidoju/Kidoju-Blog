709af7b13a402b0a9575f699b8998172d3959a86
jlchereau <jlchereau@msn.com>
2018-12-29 17:12:27 +0000

    Upgraded webdriverio from v4 to v5

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+// const path = require('path');
+
 /** *************************************************************
  * On any platform including Travis-CI
  ************************************************************** */
@@ -5,34 +7,42 @@
 let capabilities = [
     {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instance available you can make sure that not more than
-        // 5 instance gets started at a time.
-        maxInstances: 5,
-        // The following are the default drivers installed with selenium-standalone
-        // browserName: 'chrome'
-        // browserName: 'firefox' // gecko
-        // browserName: 'internet explorer'
-        // The following driver is installed with phantomjs-brebuilt
-        browserName: 'phantomjs'
+        // grid with only 5 firefox instances available you can make sure that not more than
+        // 5 instances get started at a time.
+        maxInstances: 1,
+        browserName: 'chrome',
+        // https://github.com/webdriverio/webdriverio/issues/3130#issuecomment-447405876
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu']
+        }
+        // unexpectedAlertBehaviour: 'ignore'
     }
 ];
-let seleniumArgs = {};
+// let seleniumArgs = {};
+const seleniumArgs = {};
 
 /** **************************************************************
  * In our Windows environment
  *************************************************************** */
 
-if (/^win/.test(process.platform)) {
+// if (/^win/.test(process.platform)) {
+if (/^oops/.test(process.platform)) {
+    // @see https://medium.com/@jlchereau/how-to-configure-webdrivier-io-with-selenium-standalone-and-additional-browsers-9369d38bc4d1
+    /*
     seleniumArgs = {
         // Drivers can be downloaded at http://docs.seleniumhq.org/download/
         javaArgs: [
             // Add Microsoft Edge driver
             // `-Dwebdriver.edge.driver=${path.join(__dirname, './test/bin/MicrosoftWebDriver.exe')}`,
-            '-Dwebdriver.edge.driver=C:\\Users\\jlche\\AppData\\Roaming\\npm\\node_modules\\selenium-standalone\\.selenium\\edgedriver\\17134-MicrosoftEdgeDriver.exe',
+            // '-Dwebdriver.edge.driver=C:\\Users\\jlche\\AppData\\Roaming\\npm\\node_modules\\selenium-standalone\\.selenium\\edgedriver\\17134-MicrosoftEdgeDriver.exe'
+            `-Dwebdriver.edge.driver=${path.join(
+                __dirname,
+                './node_modules/selenium-standalone/.selenium/edgedriver/17134-MicrosoftEdgeDriver.exe'
+            )}`
 
             // Add opera driver
             // `-Dwebdriver.opera.driver=${path.join(__dirname, './test/bin/operadriver.exe')}`,
-            '-Dwebdriver.opera.driver=C:\\Users\\jlche\\AppData\\Roaming\\npm\\node_modules\\selenium-standalone\\.selenium\\chromedriver\\2.41-x64-chromedriver'
+            // '-Dwebdriver.opera.driver=C:\\Users\\jlche\\AppData\\Roaming\\npm\\node_modules\\selenium-standalone\\.selenium\\chromedriver\\2.43-x64-chromedriver'
         ]
         // For other opts, see https://github.com/vvo/selenium-standalone/blob/master/lib/start.js#L22
         // seleniumArgs: [],
@@ -42,64 +52,63 @@ if (/^win/.test(process.platform)) {
         // basePath
         // javaPath
     };
+    */
     capabilities = [
-        // @see https://medium.com/@jlchereau/how-to-configure-webdrivier-io-with-selenium-standalone-and-additional-browsers-9369d38bc4d1
         {
             maxInstances: 1,
             browserName: 'chrome'
-            // unexpectedAlertBehaviour: 'ignore' - use it to display an alert that is not seen
+            // 'goog:chromeOptions': {}
         },
         {
             maxInstances: 1,
-            browserName: 'firefox',
-            // @see https://github.com/vvo/selenium-standalone/issues/237
-            // @see http://stackoverflow.com/questions/38125581/how-to-enable-a-firefox-extension-when-testing-with-selenium-webdriverio
-            marionette: true
+            browserName: 'firefox'
+            // 'moz:firefoxOptions': { args: ['-headless'] }
         },
         {
             maxInstances: 1,
             browserName: 'internet explorer'
-        },
+            // 'se:ieOptions': {}
+        } /* ,
         {
+            // See https://github.com/webdriverio/webdriverio/issues/3196
             maxInstances: 1,
-            browserName: 'MicrosoftEdge'
+            browserName: 'MicrosoftEdge',
+            osPlatform: 'Windows',
+            osVersion: '10'
         },
+        // The following driver is installed with phantomjs-prebuilt
         {
+            // See https://github.com/webdriverio/webdriverio/issues/3198
             maxInstances: 1,
             browserName: 'phantomjs',
             // Without the path, phantomJS is not found on Windows
-            // 'phantomjs.binary.path': path.join(__dirname, './node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs.exe')
-            'phantomjs.binary.path':
-                'C:\\Program Files (x86)\\PhantomJS\\bin\\phantomjs.EXE'
+            // 'phantomjs.binary.path': 'C:\\Program Files (x86)\\PhantomJS\\bin\\phantomjs.EXE'
+            'phantomjs.binary.path': path.join(__dirname, './node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs.exe')
         },
         {
             maxInstances: 1,
             browserName: 'operablink',
-            chromeOptions: {
+            'goog:chromeOptions': {
                 args: [],
                 extensions: [],
                 // binary: 'C:\\Program Files (x86)\\Opera\\launcher.exe'
                 binary:
-                    'C:\\Program Files (x86)\\Opera\\53.0.2907.106\\opera.exe'
+                    'C:\\Program Files (x86)\\Opera\\57.0.3098.106\\opera.exe'
             }
         }
+        */
     ];
 }
 
 module.exports.config = {
-    // =====================
-    // Server Configurations
-    // =====================
-    // Host address of the running Selenium server. This information is usually obsolete as
-    // WebdriverIO automatically connects to localhost. Also, if you are using one of the
-    // supported cloud services like Sauce Labs, Browserstack, or Testing Bot you don't
-    // need to define host and port information because WebdriverIO can figure that out
-    // according to your user and key information. However, if you are using a private Selenium
-    // backend you should define the host address, port, and path here.
     //
-    // host: '0.0.0.0',
-    // port: 4444,
-    // path: '/wd/hub',
+    // ====================
+    // Runner Configuration
+    // ====================
+    //
+    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
+    // on a remote machine).
+    runner: 'local',
     //
     // =================
     // Service Providers
@@ -108,8 +117,13 @@ module.exports.config = {
     // should work too though). These services define specific user and key (or access key)
     // values you need to put in here in order to connect to these services.
     //
-    // user: 'webdriverio',
-    // key:  'xxxxxxxxxxxxxxxx-xxxxxx-xxxxx-xxxxxxxxx',
+    // user: process.env.SAUCE_USERNAME,
+    // key: process.env.SAUCE_ACCESS_KEY,
+    //
+    // If you run your tests on SauceLabs you can specify the region you want to run your tests
+    // in via the `region` property. Available short handles for regions are:
+    // us: us-west-1 (default)
+    // eu: eu-central-1
     //
     // ==================
     // Specify Test Files
@@ -119,7 +133,7 @@ module.exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
-    specs: ['./test/selenium/**/*.js'],
+    specs: ['./test/selenium/**/*.test.es6'],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
@@ -148,6 +162,7 @@ module.exports.config = {
     //
     capabilities, // See above
     //
+    // TODO BEGIN Check (was in webdriver v4 config, might not exist in v5)
     // When enabled opens a debug port for node-inspector and pauses execution
     // on `debugger` statements. The node-inspector can be attached with:
     // `node-inspector --debug-port 5859 --no-preload`
@@ -158,28 +173,37 @@ module.exports.config = {
     //
     // Additional list node arguments to use when starting child processes
     // execArgv: null,
+    // TODO END
     //
     // ===================
     // Test Configurations
     // ===================
     // Define all options that are relevant for the WebdriverIO instance here
+    // More information at https://webdriver.io/docs/options.html
     //
+    // TODO BEGIN Check (was in webdriver v4 config, might not exist in v5)
     // By default WebdriverIO commands are executed in a synchronous way using
     // the wdio-sync package. If you still want to run your tests in an async way
     // e.g. using promises you can set the sync option to false.
-    sync: true,
+    // sync: false,
+    // TODO END
     //
-    // Level of logging verbosity: silent | verbose | command | data | result | error
-    logLevel: 'verbose',
+    // Level of logging verbosity: trace | debug | info | warn | error
+    logLevel: 'info',
     //
-    // Enables colors for log output.
-    coloredLogs: true,
+    // Warns when a deprecated command is used
+    deprecationWarnings: true,
     //
+    // If you only want to run your tests until a specific amount of tests have failed use
+    // bail (default is 0 - don't bail, run all tests).
+    bail: 0,
     // Saves a screenshot to a given path if a command fails.
     screenshotPath: './errorShots/',
     //
-    // Set a base URL in order to shorten url command calls. If your url parameter starts
-    // with "/", then the base url gets prepended.
+    // Set a base URL in order to shorten url command calls. If your `url` parameter starts
+    // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
+    // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
+    // gets prepended directly.
     baseUrl: 'http://localhost:3000',
     //
     // Default timeout for all waitFor* commands.
@@ -215,14 +239,15 @@ module.exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     // services: [],//
-    services: ['selenium-standalone', 'phantomjs'],
+    // services: ['selenium-standalone', 'phantomjs'],
+    services: ['selenium-standalone'],
     // selenium-standalone configuration
     // @see http://webdriver.io/guide/services/selenium-standalone.html
     // @see https://www.npmjs.com/package/selenium-standalone
     seleniumArgs,
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
-    // see also: http://webdriver.io/guide/testrunner/frameworks.html
+    // see also: http://webdriver.io/docs/frameworks.html
     //
     // Make sure you have the wdio adapter package for the specific framework installed
     // before running any tests.
@@ -230,14 +255,16 @@ module.exports.config = {
     //
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
-    // see also: http://webdriver.io/guide/testrunner/reporters.html
+    // see also: http://webdriver.io/docs/dot-reporter.html
     // reporters: ['dot'],
+    reporters: ['spec'],
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000 // for gremlins
+        timeout: 60000, // for gremlins
+        require: ['js:@babel/register']
     },
     //
     // =====
@@ -247,58 +274,108 @@ module.exports.config = {
     // it and to build services around it. You can either apply a single function or an array of
     // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
     // resolved to continue.
-    //
-    // Gets executed once before all workers get launched.
-    // onPrepare: function (config, capabilities) {}
+    /**
+     * Gets executed once before all workers get launched.
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     */
+    // onPrepare: function (config, capabilities) {
+    // },
     // Start the web application
     onPrepare: () => require('./webapp/server') // eslint-disable-line global-require
-    //
-    // Gets executed before test execution begins. At this point you can access all global
-    // variables, such as `browser`. It is the perfect place to define custom commands.
+    /**
+     * Gets executed just before initialising the webdriver session and test framework. It allows you
+     * to manipulate configurations depending on the capability or spec.
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that are to be run
+     */
+    // beforeSession: function (config, capabilities, specs) {
+    // },
+    /**
+     * Gets executed before test execution begins. At this point you can access to all global
+     * variables like `browser`. It is the perfect place to define custom commands.
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that are to be run
+     */
     // before: function (capabilities, specs) {
     // },
-    //
-    // Hook that gets executed before the suite starts
-    // beforeSuite: function (suite) {
-    // },
-    //
-    // Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
-    // beforeEach in Mocha)
-    // beforeHook: function () {
-    // },
-    //
-    // Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
-    // afterEach in Mocha)
-    // afterHook: function () {
-    // },
-    //
-    // Function to be executed before a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
-    // beforeTest: function (test) {
-    // },
-    //
-    // Runs before a WebdriverIO command gets executed.
+    /**
+     * Runs before a WebdriverIO command gets executed.
+     * @param {String} commandName hook command name
+     * @param {Array} args arguments that command would receive
+     */
     // beforeCommand: function (commandName, args) {
     // },
-    //
-    // Runs after a WebdriverIO command gets executed
-    // afterCommand: function (commandName, args, result, error) {
+    /**
+     * Hook that gets executed before the suite starts
+     * @param {Object} suite suite details
+     */
+    // beforeSuite: function (suite) {
     // },
-    //
-    // Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+    /**
+     * Function to be executed before a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+     * @param {Object} test test details
+     */
+    // beforeTest: function (test) {
+    // },
+    /**
+     * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
+     * beforeEach in Mocha)
+     */
+    // beforeHook: function () {
+    // },
+    /**
+     * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
+     * afterEach in Mocha)
+     */
+    // afterHook: function () {
+    // },
+    /**
+     * Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+     * @param {Object} test test details
+     */
     // afterTest: function (test) {
     // },
-    //
-    // Hook that gets executed after the suite has ended
+    /**
+     * Hook that gets executed after the suite has ended
+     * @param {Object} suite suite details
+     */
     // afterSuite: function (suite) {
     // },
-    //
-    // Gets executed after all tests are done. You still have access to all global variables from
-    // the test.
+    /**
+     * Runs after a WebdriverIO command gets executed
+     * @param {String} commandName hook command name
+     * @param {Array} args arguments that command would receive
+     * @param {Number} result 0 - command success, 1 - command error
+     * @param {Object} error error object if any
+     */
+    // afterCommand: function (commandName, args, result, error) {
+    // },
+    /**
+     * Gets executed after all tests are done. You still have access to all global variables from
+     * the test.
+     * @param {Number} result 0 - test pass, 1 - test fail
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that ran
+     */
     // after: function (result, capabilities, specs) {
     // },
-    //
-    // Gets executed after all workers got shut down and the process is about to exit. It is not
-    // possible to defer the end of the process using a promise.
-    // onComplete: function(exitCode) {
+    /**
+     * Gets executed right after terminating the webdriver session.
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {Array.<String>} specs List of spec file paths that ran
+     */
+    // afterSession: function (config, capabilities, specs) {
+    // },
+    /**
+     * Gets executed after all workers got shut down and the process is about to exit.
+     * @param {Object} exitCode 0 - success, 1 - fail
+     * @param {Object} config wdio configuration object
+     * @param {Array.<Object>} capabilities list of capabilities details
+     * @param {<Object>} results object containing test results
+     */
+    // onComplete: function(exitCode, config, capabilities, results) {
     // }
 };

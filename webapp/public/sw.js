2f02e288e22425ca852bdf0225bb9e11e7268e67
jlchereau <jlchereau@msn.com>
2019-07-09 15:02:19 +0100

    New production build

--- webapp/public/sw.js
+++ webapp/public/sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://www.kidoju.com/support/build/precache-manifest.ee9126ee4b6eb32cd723dce8b2bff0fc.js"
+  "https://www.kidoju.com/support/build/precache-manifest.c4feeee265f4b8f2202b72d479f479dd.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Kidoju-Blog"});
